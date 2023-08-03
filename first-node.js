fc73d53a6ba8560b9f880d6d7be15805a96a67b4
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-08-03 23:20:41 +0530

    print a to e using async await

--- first-node.js
+++ first-node.js
@@ -1,9 +1,47 @@
-const fruits = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// console.log('a');
 
-console.log(fruits);
-console.log(fruits.map(fruit => {
-    if(fruit === ' ')
-        fruit = 'empty string';
+// console.log('b');
 
-    return fruit;
-}));
+// setTimeout(() => console.log('c'), 3000)
+
+// setTimeout(() => console.log('d'), 0)
+
+// console.log('e');
+
+async function print()
+{
+    const printA = new Promise((resolve,reject) => {
+        resolve(console.log('a'))
+    })
+
+    const printB = printA.then(() => {
+        return new Promise((resolve,reject) => {
+            resolve(console.log('b'))
+        })
+    })
+    const printC = printB.then(() => {
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(console.log('c'))
+            },3000)
+        })
+    })
+    const printD = printC.then(() => {
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(console.log('d'))
+            },0)
+        })
+    })
+    const printE = printD.then(() => {
+        return new Promise((resolve,reject) => {
+            resolve(console.log('e'));
+        })
+    })
+    let a = await printA;
+    let b = await printB;
+    let c = await printC;
+    let d = await printD;
+    return a;
+}
+print();
