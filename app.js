e68cfc8f6ae02598732aa4c7f0065d2be4416924
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-08-04 18:44:52 +0530

    giving response page according to change in url

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
  
 const server = http.createServer((req,res) => {
-    console.log('Trishala');
+    if(req.url==='/home')
+        res.write('Welcome Home');
+    else if(req.url==='/about')
+        res.write('Welcome to About Us page');
+    else if(req.url==='/node')
+        res.write('Welcome to my Node Js project');
+
+    res.end();
 })
 
 server.listen(4000);
