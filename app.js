2ff7dd975ce6c9f9a159a3b9e8d38eb062d8db10
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-08-04 21:52:47 +0530

    working with file read and write

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const http = require('http');
+const fs = require('fs');
  
 const server = http.createServer((req,res) => {
-    if(req.url==='/home')
-        res.write('Welcome Home');
-    else if(req.url==='/about')
-        res.write('Welcome to About Us page');
-    else if(req.url==='/node')
-        res.write('Welcome to my Node Js project');
-
-    res.end();
+    const url = req.url;
+    const method = req.method;
+    if(url === '/')
+    {
+        try{
+            const data = fs.readFileSync('message.txt');
+            res.write('<html>');
+            res.write('<head><title>My page</title></head>');
+            res.write(`<body>${data.toString()}<form action="/message" method="POST"><input type="text" name="message"><br><button type="submit">SEND</button></form></body>`);
+            res.write('</html>');
+            return res.end();
+        }
+        catch(err){
+            console.log(err);
+        }
+        
+    }
+    if(url === '/message' && method==='POST')
+    {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.appendFile('message.txt', message, (err) => {
+                if(err)
+                    console.log(err);
+            });
+        })
+        res.statusCode=302;
+        res.setHeader('location','/');
+        return res.end();
+    }
+        
 })
 
 server.listen(4000);
